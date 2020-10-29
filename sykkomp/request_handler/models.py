c6c5d73ed6ce249ca9672605200ea79176f328ac
simoneid <simoneid@stud.ntnu.no>
2020-10-29 10:18:43 +0100

    fixing .gitignore

--- sykkomp/request_handler/models.py
+++ sykkomp/request_handler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CycleTimes(models.Model):
+    last_data = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.last_data
--- sykkomp/request_handler/views.py
+++ sykkomp/request_handler/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import CycleTimes
 
 # Create your views here.
 
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        return render(request, "request_handler/index.html", data)
+        request_data = json.loads(request.body)
+        ct = CycleTimes(last_data=request_data["time"])
+        data = {"time": ct.last_data}
+        print("Request Data:", request_data)
+        print("Persistent Data: ", data)
     else:
-        return HttpResponse("2. Get request")
+        ct = CycleTimes()
+        data = {"time": ct.last_data}
+    return HttpResponse(data)
